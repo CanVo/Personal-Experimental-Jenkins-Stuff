eae04096956c9fc91c061dcd3038a02f8583f037
CanVo <canvo1997@gmail.com>
2019-01-02 12:28:33 -0600

    Test

---
+++ helloworld/src/main/java/io/jenkins/plugins/sample/PowerShell.java
@@ -0,0 +1,69 @@
+package hudson.plugins.powershell;
+
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.model.AbstractProject;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Builder;
+import hudson.tasks.CommandInterpreter;
+import org.apache.commons.lang.SystemUtils;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * Invokes Windows PowerShell from Jenkins.
+ * 
+ * @author Kohsuke Kawaguchi
+ */
+public class PowerShell extends CommandInterpreter {
+    @DataBoundConstructor
+    public PowerShell(String command) {
+        super(command);
+    }
+
+    protected String getFileExtension() {
+        return ".ps1";
+    }
+
+    public String[] buildCommandLine(FilePath script) {
+        if (isRunningOnWindows(script)) {
+            return new String[] { "powershell.exe", "-NonInteractive", "-ExecutionPolicy", "Bypass", "& \'" + script.getRemote() + "\'"};
+        } else {
+            // ExecutionPolicy option does not work (and is not required) for non-Windows platforms
+            // See https://github.com/PowerShell/PowerShell/issues/2742
+            return new String[] { "pwsh", "-NonInteractive", "& \'" + script.getRemote() + "\'"};
+        }
+    }
+
+    protected String getContents() {
+        return command + "\r\nexit $LastExitCode";
+    }
+
+    private boolean isRunningOnWindows(FilePath script) {
+        // Ideally this would use a property of the build/run, but unfortunately CommandInterpreter only gives us the
+        // FilePath, so we need to guess based on that.
+        if (!script.isRemote()) {
+            // Running locally, so we can just check the local OS
+            return SystemUtils.IS_OS_WINDOWS;
+        }
+
+        // Running remotely, guess based on the path. A path starting with something like "C:\" is Windows.
+        String path = script.getRemote();
+        return path.length() > 3 && path.charAt(1) == ':' && path.charAt(2) == '\\';
+    }
+
+    @Extension
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+        @Override
+        public String getHelpFile() {
+            return "/plugin/powershell/help.html";
+        }
+        
+        public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+            return true;
+        }
+
+        public String getDisplayName() {
+            return "Windows PowerShell";
+        }
+    }
+}
