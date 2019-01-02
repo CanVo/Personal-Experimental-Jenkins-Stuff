f8ea921a88744817cbfd7dc725547127a0bc0e29
CanVo <canvo1997@gmail.com>
2019-01-02 12:57:27 -0600

    test

--- helloworld/src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
+++ helloworld/src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
@@ -21,9 +21,7 @@ import org.kohsuke.stapler.DataBoundSetter;
 public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
 
     private final String name;
-    
     private final String settingsName, scanName, startUrls, crawlAuditMode, sharedThreads, crawlThreads, auditThreads, startOption, loginMacro, workFlowMacros, tcMarcoParameters, smartCredentials, networkCredentials, networkAuthenticationMode, allowedHosts, policyID, checkIDs, dontStartScan, scanScope, scopedPaths, clientCertification, storeName, isGlobal, serialNumber, bytes, reuseScan, scanId, mode;
-    
     private boolean useFrench;
 
     @DataBoundConstructor
@@ -114,9 +112,6 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
         	
         	return FormValidation.ok();
         }
-        
-        
-        
         
         
         
