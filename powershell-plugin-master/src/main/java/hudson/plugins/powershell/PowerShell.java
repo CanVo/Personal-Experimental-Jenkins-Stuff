2c45a4d7731ecf0c02925e1b82792df2db66f66d
CanVo <canvo1997@gmail.com>
2019-01-07 11:54:56 -0600

    test

--- powershell-plugin-master/src/main/java/hudson/plugins/powershell/PowerShell.java
+++ powershell-plugin-master/src/main/java/hudson/plugins/powershell/PowerShell.java
@@ -149,7 +149,7 @@ public class PowerShell extends CommandInterpreter {
     	// Default Scan:
     	// Invoke-RestMethod -Uri http://localhost:8083/webinspect/scanner/scans -Method Post -ContentType 'application/json' -Body '{ "settingsName": "Default" }'
     	// String memes = "\"{\"settingsName\":\"" + settingsName + "\", \"overrides\":{\"scanName\":\"PLSWORK!\"}}\"";
-    	String memes = "@'{ \"settingsName\": " + settingsName + "} '@";
+    	String memes = "@'`n{ \"settingsName\": " + settingsName + "} `n'@";
     	return "Invoke-RestMethod -Uri http://" + ipInstance + ":" + scanPort + "/webinspect/scanner/scans -Method Post -ContentType 'application/json' -Body " + memes;
     	
     	
