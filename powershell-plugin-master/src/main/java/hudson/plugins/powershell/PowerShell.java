f1205421bdd10b6afcfc6390d2c5a2f2a187fffa
CanVo <canvo1997@gmail.com>
2019-01-09 08:58:34 -0600

    test

--- powershell-plugin-master/src/main/java/hudson/plugins/powershell/PowerShell.java
+++ powershell-plugin-master/src/main/java/hudson/plugins/powershell/PowerShell.java
@@ -217,7 +217,8 @@ public class PowerShell extends CommandInterpreter {
     	// Default Scan:
     	// Invoke-RestMethod -Uri http://localhost:8083/webinspect/scanner/scans -Method Post -ContentType 'application/json' -Body '{ "settingsName": "Default" }'
     	//return "Invoke-RestMethod -Uri http://" + ipInstance + ":" + scanPort + "/webinspect/scanner/scans -Method Post -ContentType 'application/json' -Body " + memes;
-    	String scanInvoke = "Write-Host Invoke-RestMethod -Uri http://" + ipInstance + ":" + scanPort + "/webinspect/scanner/scans -Method Post -ContentType 'application/json' -Body \'{\"settingsName\":\"" + settingsName + "\", \"overrides\":" + scan + "\'";
+    	//String scanInvoke = "Write-Host Invoke-RestMethod -Uri http://" + ipInstance + ":" + scanPort + "/webinspect/scanner/scans -Method Post -ContentType 'application/json' -Body \'{ \"settingsName\":\"" + settingsName + "\", \"overrides\":" + scan + "\'";
+    	String scanInvoke = "Write-Host " + scan;
     	return scanInvoke;
     }
 
